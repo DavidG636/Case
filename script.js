27abb867c03a065130532e2369a8eac2a64d4b7f
David Gonzalez <gonzalezdavid636@gmail.com>
2018-08-16 21:09:15 -0400

    Add equal sign in two if statements

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ $(function() {
   var change;
   $(".submitText").click(function() {
     state = $('.slider').css('background-color');
-    state = new String (state);
-    console.log(state);
+    state = new String(state);
 
     let textContent = $('#text').val();
     var changedText;
 
-    if (state = 'rgb(42, 185, 52)') {
+    if (state == 'rgb(42, 185, 52)') {
       change = 'capital';
     } else if (state == "rgb(202, 34, 34)") {
       change = 'lower';
@@ -19,8 +18,7 @@ $(function() {
       changedText = textContent.toUpperCase();
       $('#text').val(changedText)
 
-    }
-    else if (change = 'lower'){
+    } else if (change == 'lower') {
       changedText = textContent.toLowerCase();
       $('#text').val(changedText)
     }
